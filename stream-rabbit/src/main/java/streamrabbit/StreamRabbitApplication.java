791ad2f4b5ac6e4c6a1f8960750b4bbbef3a2a4e
wei.peng <wei.peng@aorise.org>
2018-09-13 18:01:59 +0800

    springcloud stream-rabbit提交

---
+++ stream-rabbit/src/main/java/streamrabbit/StreamRabbitApplication.java
@@ -0,0 +1,12 @@
+package streamrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamRabbitApplication.class, args);
+	}
+}
