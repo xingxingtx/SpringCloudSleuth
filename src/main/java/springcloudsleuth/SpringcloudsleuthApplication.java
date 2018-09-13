210db3feb3ca603bdd0b66de6c9e2adb0d3c17bb
wei.peng <wei.peng@aorise.org>
2018-09-13 09:43:40 +0800

    first commit

---
+++ src/main/java/springcloudsleuth/SpringcloudsleuthApplication.java
@@ -0,0 +1,12 @@
+package springcloudsleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudsleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudsleuthApplication.class, args);
+	}
+}
---
+++ src/test/java/springcloudsleuth/SpringcloudsleuthApplicationTests.java
@@ -0,0 +1,16 @@
+package springcloudsleuth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudsleuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
