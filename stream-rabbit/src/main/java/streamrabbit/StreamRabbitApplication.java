ddefe5e9e2be2ff3453e5759fbe224b3b6a5a78b
wei.peng <wei.peng@aorise.org>
2018-09-14 10:30:54 +0800

    springcloud stream-rabbit提交

--- stream-rabbit/src/main/java/streamrabbit/StreamRabbitApplication.java
+++ stream-rabbit/src/main/java/streamrabbit/StreamRabbitApplication.java
@@ -2,8 +2,12 @@ package streamrabbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinStreamServer
 public class StreamRabbitApplication {
 
 	public static void main(String[] args) {
